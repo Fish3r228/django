6ac84042981ce318ee90a26705a18809beea1095
Fish3r228 <zayarobert30@gmail.com>
2025-06-22 11:07:20 +0300

    Fix: product detail view, media setup, ignore

--- catalog/views.py
+++ catalog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Product
 
 # Create your views here.
@@ -12,12 +12,11 @@ def index(request):
     products = Product.objects.all()
     return render(request, 'index.html', {'products': products})
 
-def index(request):
-    products = Product.objects.all()
-    return render(request, 'index.html', {'products': products})
-
-
 def product_list(request):
 
     return render(request, 'catalog/product_list.html', context={})
 
+
+def product_detail(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+    return render(request, 'product_detail.html', {'product': product})
