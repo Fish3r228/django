42051b79c8d5da9f61652110113dc7714a64bc36
Fish3r228 <zayarobert30@gmail.com>
2025-06-28 14:01:17 +0300

    Hw_fix1

---
+++ blogs/views.py
@@ -0,0 +1,48 @@
+# blogs/views.py
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+from .models import BlogPost
+
+
+class BlogListView(ListView):
+    model = BlogPost
+    template_name = 'blogs/blog_list.html'
+    context_object_name = 'posts'
+
+    def get_queryset(self):
+        return BlogPost.objects.filter(is_published=True)
+
+
+class BlogDetailView(DetailView):
+    model = BlogPost
+    template_name = 'blogs/blog_detail.html'
+    context_object_name = 'post'
+
+    def get_object(self, queryset=None):
+        obj = super().get_object(queryset)
+        obj.views_count += 1
+        obj.save(update_fields=['views_count'])
+        return obj
+
+
+class BlogCreateView(CreateView):
+    model = BlogPost
+    fields = ['title', 'content', 'is_published']
+    template_name = 'blogs/blog_form.html'
+    success_url = reverse_lazy('blog_list')
+
+
+class BlogUpdateView(UpdateView):
+    model = BlogPost
+    fields = ['title', 'content', 'is_published']
+    template_name = 'blogs/blog_form.html'
+
+    def get_success_url(self):
+        return self.object.get_absolute_url()
+
+
+class BlogDeleteView(DeleteView):
+    model = BlogPost
+    template_name = 'blogs/blog_confirm_delete.html'
+    success_url = reverse_lazy('blog_list')
+
